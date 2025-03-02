863b7f106acbd1ed5de11dcbd225e8152feb66e0
Samuel Drai <samuel.drai@hey.com>
2025-03-02 22:57:34 +0100

    chore: clean

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,21 +25,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-black">
-      {/* <Head>
-        <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <link
-          rel="icon"
-          href="favicon-light.ico"
-          type="image/x-icon"
-          media="(prefers-color-scheme: light)"
-        />
-        <link
-          rel="icon"
-          href="favicon.ico"
-          type="image/x-icon"
-          media="(prefers-color-scheme: dark)"
-        />
-      </Head> */}
       <body className="antialiased Root">{children}</body>
     </html>
   )
