79130070ae1af081836e5352cfd300362c8136d0
Samuel Drai <samuel.drai@hey.com>
2025-03-03 16:33:20 +0100

    feat

--- src/components/switch.tsx
+++ src/components/switch.tsx
@@ -9,7 +9,6 @@ export function Switch() {
   const offRef = useRef<HTMLAudioElement>(null)
 
   useEffect(() => {
-    if (initialRender) setInitialRender(false)
     if (checked) {
       document.documentElement.classList.remove("dark")
       if (!initialRender) onRef.current?.play()
@@ -17,6 +16,7 @@ export function Switch() {
       document.documentElement.classList.add("dark")
       if (!initialRender) offRef.current?.play()
     }
+    if (initialRender) setInitialRender(p => !p)
   }, [checked])
 
   return (
@@ -24,6 +24,7 @@ export function Switch() {
       <audio ref={onRef} src="/on.mp3" style={{ display: "none" }} />
       <audio ref={offRef} src="/off.mp3" style={{ display: "none" }} />
       <SwitchBase.Root
+        defaultChecked={checked}
         checked={checked}
         onCheckedChange={() => setChecked(p => !p)}
         className="relative flex h-4 w-7 rounded-full bg-gradient-to-r from-gray-700 from-35% to-gray-400 to-65% bg-[length:6.5rem_100%] bg-[100%_0%] bg-no-repeat p-px outline-1 -outline-offset-1 outline-transparent transition-[background-position,box-shadow] duration-[125ms] ease-[cubic-bezier(0.26,0.75,0.38,0.45)] before:absolute before:rounded-full before:outline-offset-2 before:outline-blue-800 focus-visible:before:inset-0 focus-visible:before:outline-2 active:bg-gray-100 data-[checked]:bg-[0%_0%] data-[checked]:active:bg-gray-800 dark:from-gray-800 dark:shadow-black/75 dark:outline-white/15 dark:data-[checked]:shadow-none"
