5ec2fd8d6ca60b8d9aa4b92ff2491965519480cf
Samuel Drai <samuel.drai@hey.com>
2025-03-13 15:53:02 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,28 @@ export const metadata: Metadata = {
       type: "image/svg+xml",
     },
   ],
+  openGraph: {
+    title: "Drysdale Ventures",
+    description: "Backing software companies from day 0",
+    url: "https://drysdale.vc",
+    siteName: "Drysdale Ventures",
+    images: [
+      {
+        url: "/opengraph-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Drysdale Ventures",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Drysdale Ventures",
+    description: "Backing software companies from day 0",
+    images: ["/opengraph-image.jpg"],
+  },
 }
 
 export default function RootLayout({
